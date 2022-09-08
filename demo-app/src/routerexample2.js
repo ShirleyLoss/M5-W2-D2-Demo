6af5ca788d8ce197402d8920550ae94e09429ecd
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-09-07 20:02:16 -0700

    Finished demo.

---
+++ demo-app/src/routerexample2.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useParams
+} from "react-router-dom";
+
+export default function App() {
+  return (
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/topics" element={<Topics />}>
+            <Route path="" element={<Info />} />
+            <Route path=":topicId" element={<Topic />} /> {/* must match with variable name in line 52 & 53 */}
+          </Route>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+
+// add topic function
+function Home(){
+  return <h2>Home</h2>
+}
+
+function About(){
+  return <h2>About</h2>
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
+
+function Info(){
+  return <h3>Please select a topic above</h3>
+}
+
+function Topics(){
+  return(
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to="cats">Cats</Link>
+        </li>
+        <li>
+          <Link to="dogs">Dogs</Link>
+        </li>
+      </ul>
+
+      {/* render child link */}
+      <Outlet />
+    </div>
+  )
+}
